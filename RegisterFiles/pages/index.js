e9542b514a6a383ed8c2030335a248878b7a342f
LucasUzoka <92759285+LucasUzoka@users.noreply.github.com>
2021-12-21 13:43:45 -0500

    Login and Register Renders
    pages and components with validation and authentication

---
+++ RegisterFiles/pages/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
+import Register from '../../components/Register';
+import RegisterSuccess from '../../components/RegisterSuccess';
+import Navbar from '../../components/Navbar';
+
+const RegisterForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <div style={{height: "100vh"}}>
+    
+      <div className='form-container'>      
+        <div className='form-content-left'>
+          <img className='form-img' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.misskatecuttables.com%2Fuploads%2Fshopping_cart%2F10600%2Flarge_donut.png&f=1&nofb=1' alt='donut' />
+        </div>
+        {!isSubmitted ? (
+          <Register />
+        ) : (
+          <RegisterSuccess />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RegisterForm;
